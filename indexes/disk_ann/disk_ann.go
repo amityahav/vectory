51246d1241a6a435b1c681484c10278614506276
Amit Yahav <amityahav12@gmail.com>
2023-06-27 22:05:17 +0300

    robustPrune

--- indexes/disk_ann/disk_ann.go
+++ indexes/disk_ann/disk_ann.go
@@ -8,7 +8,7 @@ type Api interface {
 	Insert(vector []float32) error
 
 	// Delete Lazy deletion of vertex v
-	Delete(v string)
+	Delete(id uint32)
 
 	// Search for K-NN by querying Long-term index, RW-index and all RO-indexes, aggregating and filtering results
 	Search(vector []float32, k int, l int)
@@ -18,11 +18,22 @@ type DiskAnn struct {
 	sync.RWMutex
 	rwIndex              *MemoryIndex
 	roIndexes            []*MemoryIndex
+	deletedIds           *sync.Map
 	memoryIndexSizeLimit int
 	ltIndex              string
 }
 
-func NewDiskAnn() {
+func NewDiskAnn() *DiskAnn {
+	da := DiskAnn{
+		deletedIds:           &sync.Map{},
+		roIndexes:            nil,
+		memoryIndexSizeLimit: 0,
+		ltIndex:              "",
+	}
+
+	da.rwIndex = newMemoryIndex(0, da.deletedIds)
+
+	return &da
 }
 
 // Load loads the index from disk and recovers in-memory indexes in case of a crash
@@ -32,8 +43,10 @@ func Load() {
 func (da *DiskAnn) Insert(vector []float32) error {
 	da.Lock()
 	if da.rwIndex.graph.size() == da.memoryIndexSizeLimit {
+		da.rwIndex.ReadOnly()
 		go da.rwIndex.Snapshot()
-		da.rwIndex = newMemoryIndex()
+		da.roIndexes = append(da.roIndexes, da.rwIndex)
+		da.rwIndex = newMemoryIndex(da.rwIndex.currId, da.deletedIds)
 	}
 	da.Unlock()
 
@@ -42,4 +55,11 @@ func (da *DiskAnn) Insert(vector []float32) error {
 		return err
 	}
 
+	return nil
+}
+
+func (da *DiskAnn) Delete(id uint32) bool {
+	da.deletedIds.Store(id, struct{}{})
+
+	return true
 }
--- indexes/disk_ann/memory_index.go
+++ indexes/disk_ann/memory_index.go
@@ -1,13 +1,16 @@
 package disk_ann
 
 import (
+	"Vectory/indexes"
+	"container/heap"
 	"sync"
 )
 
 type MemoryIndex struct {
 	sync.RWMutex
 	graph             *Graph
-	calculateDistance func([]float32, float32) float32
+	calculateDistance func([]float32, []float32) float32
+	deletedIds        *sync.Map
 
 	// search list size
 	l int
@@ -18,16 +21,18 @@ type MemoryIndex struct {
 	// starting point
 	s uint32
 
-	lastInsertedId uint32
+	currId uint32
 
 	readOnly     bool
 	snapshotPath string
 }
 
-func newMemoryIndex() *MemoryIndex {
+func newMemoryIndex(currId uint32, deletedIds *sync.Map) *MemoryIndex {
 	mi := MemoryIndex{
 		graph:             nil,
 		calculateDistance: nil,
+		deletedIds:        deletedIds,
+		currId:            currId,
 		readOnly:          false,
 		snapshotPath:      "",
 	}
@@ -50,10 +55,10 @@ func (mi *MemoryIndex) Insert(v []float32) error {
 
 	_, visited := mi.Search(v, 1)
 
-	mi.lastInsertedId += 1
+	mi.currId += 1
 
 	vertex := Vertex{
-		Id:     mi.lastInsertedId,
+		Id:     mi.currId,
 		Vector: v,
 	}
 
@@ -74,14 +79,72 @@ func (mi *MemoryIndex) Insert(v []float32) error {
 	return nil
 }
 
-func (mi *MemoryIndex) Delete(deleteList []uint32) error {
+func (mi *MemoryIndex) Delete(id uint32) error {
+	// TODO: delete consolidation?
 	return nil
 }
 
 func (mi *MemoryIndex) RobustPrune(v *Vertex, candidates []uint32) []uint32 {
-	return nil
+	// TODO locking
+	candidateDistances := make([]indexes.Element, 0, len(candidates)+len(v.Neighbors))
+	deletedCandidates := map[uint32]struct{}{}
+
+	for _, c := range candidates {
+		if c == v.Id {
+			continue
+		}
+
+		cVertex := mi.graph.vertices[c]
+		e := indexes.Element{
+			Id:       int64(c),
+			Distance: mi.calculateDistance(v.Vector, cVertex.Vector),
+		}
+
+		candidateDistances = append(candidateDistances, e)
+	}
+
+	for _, n := range v.Neighbors {
+		nVertex := mi.graph.vertices[n]
+		e := indexes.Element{
+			Id:       int64(n),
+			Distance: mi.calculateDistance(v.Vector, nVertex.Vector),
+		}
+
+		candidateDistances = append(candidateDistances, e)
+	}
+
+	candidatesHeap := indexes.NewMinHeapFromSlice(candidateDistances)
+	newNeighbors := make([]uint32, 0, mi.graph.maxDegree)
+
+	for candidatesHeap.Len() != 0 {
+		min := heap.Pop(candidatesHeap).(indexes.Element)
+		if _, ok := deletedCandidates[uint32(min.Id)]; ok {
+			continue
+		}
+
+		newNeighbors = append(newNeighbors, uint32(min.Id))
+
+		if len(v.Neighbors) == mi.graph.maxDegree {
+			break
+		}
+
+		for _, c := range candidatesHeap.Elements {
+			minVertex := mi.graph.vertices[uint32(min.Id)]
+			cVertex := mi.graph.vertices[uint32(c.Id)]
+
+			if float32(mi.a)*mi.calculateDistance(minVertex.Vector, cVertex.Vector) <= c.Distance {
+				deletedCandidates[cVertex.Id] = struct{}{}
+			}
+		}
+	}
+
+	return newNeighbors
 }
 
 func (mi *MemoryIndex) Snapshot() {
 
+}
+
+func (mi *MemoryIndex) ReadOnly() {
+	mi.readOnly = true
 }
